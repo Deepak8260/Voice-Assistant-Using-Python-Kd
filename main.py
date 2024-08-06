49ff673f9670ae1e17ce324381868c3e5cc2cb8d
Kumar <kd8260@gmail.com>
2024-08-06 00:21:27 -0700

    successful of speechtx() function which takes manual text input and convert into voice and read it

--- main.py
+++ main.py
@@ -18,5 +18,16 @@ def sptext():
         except sr.UnknownValueError:
             print('Not Understanding')
             
-sptext()
+#sptext()
     
+
+def speechtx(x):
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice',voices[1].id)
+    rate = engine.getProperty('rate')
+    engine.setProperty('rate',120)
+    engine.say(x)
+    engine.runAndWait()
+    
+speechtx('Hello, Welcome to the World of A-I')
